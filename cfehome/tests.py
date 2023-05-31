9662810888af275a64805c8471753ea4eb80addb
AuroraHolding <122390212+AuroraHolding@users.noreply.github.com>
2023-05-30 22:33:13 -0400

    Update tests.py

--- cfehome/tests.py
+++ cfehome/tests.py
@@ -6,6 +6,10 @@ class BasicTest(TestCase):
         self.assertFalse(settings.DEBUG)
         
     def test_admin_homepage(self):
+        r = self.client.get('/admin', follow=True)
+        self.assertEqual(r.status_code, 200)
+        
+    def test_homepage(self):
         r = self.client.get('/')
         self.assertEqual(r.status_code, 200)
     
